cad38f87292e53333ce6dc6959921d49090ed6e1
Rishabpuranika <68911202+rishabpuranika@users.noreply.github.com>
2024-01-15 16:14:31 +0530

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,14 @@
+import random               #importing random module
+from passwordgeneratorfunctions import *    #importing all the functions from passwordgeneratorfunctions.py
+
+
+#main program
+print('*'*30)
+print("Welcome to the password generator")
+print('*'*30)
+main1()
+f = input("Do you want to continue? (Enter y or n): ")
+if f.lower() == "y":
+    main1()
+else:
+    exit()
